8b86a82a71c1bdec3047225a32a1d0553237c5ad
Jo <Jo@DESKTOP-FPJU820>
2017-01-31 22:26:10 +0100

    [feat] CodeGenerator from model in progress

--- GiTaigHub/src/main/java/com/ww/model/StructuredAttribut.java
+++ GiTaigHub/src/main/java/com/ww/model/StructuredAttribut.java
@@ -1,18 +1,18 @@
 package com.ww.model;
 
 public class StructuredAttribut {
-	private Class<?> type;
+	private String type;
 	private String name;
 	
-	public StructuredAttribut(Class<?> type, String name) {
+	public StructuredAttribut(String type, String name) {
 		super();
 		this.type = type;
 		this.name = name;
 	}
-	public Class<?> getType() {
+	public String getType() {
 		return type;
 	}
-	public void setType(Class<?> type) {
+	public void setType(String type) {
 		this.type = type;
 	}
 	public String getName() {
--- GiTaigHub/src/main/java/com/ww/model/StructuredClass.java
+++ GiTaigHub/src/main/java/com/ww/model/StructuredClass.java
@@ -51,6 +51,26 @@ public class StructuredClass {
 		return methods;
 	}
 
+	public List<StructuredConstrutor> getConstructors() {
+		return constructors;
+	}
+
+	public void setConstructors(List<StructuredConstrutor> constructors) {
+		this.constructors = constructors;
+	}
+
+	public List<StructuredAttribut> getAttributs() {
+		return attributs;
+	}
+
+	public void setAttributs(List<StructuredAttribut> attributs) {
+		this.attributs = attributs;
+	}
+
+	public void setMethods(List<StructuredMethod> methods) {
+		this.methods = methods;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
