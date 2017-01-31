24a6ef2b25d0f2619e1ba87eefcee10dae4457d2
Jo <Jo@DESKTOP-FPJU820>
2017-01-31 11:25:41 +0100

    [fix] Appel WS Taiga UserStory

--- GiTaigHub/src/main/java/com/ww/core/Main.java
+++ GiTaigHub/src/main/java/com/ww/core/Main.java
@@ -7,8 +7,6 @@ public class Main {
 		String passwordTaiga = args[1];
 		String projectTaiga = args[2];
 		TaigaService service = new TaigaService(usernameTaiga, passwordTaiga, projectTaiga);
-		//TaigaUserStory us = service.getUserStory("1");
-		//System.out.println(us.getDescription());
 		System.out.println(service.getUserStory("1").getDescription());
 	}
 
