40646bdf7abb259c71cda38d6187f20fcdfc99e4
FranckW <franck.warlouzet@hotmail.fr>
2017-02-02 21:15:49 +0100

    Full parsing is ok for readme example

--- GiTaigHub/src/main/java/us_parser/UserStoryParser.java
+++ GiTaigHub/src/main/java/us_parser/UserStoryParser.java
@@ -17,17 +17,20 @@ public class UserStoryParser {
 	private StructuredUserStory structuredUserStory;
 	private static String classPattern = "([A-Z][a-z0-9]+)+";
 	Pattern classNamePattern = Pattern.compile(classPattern);
-	
+	private StructuredClass userStoryClass;
+	private String userStoryArgClassName;
+
 	public void parseUserStory(TaigaUserStory taigaUserStory) {
 		structuredUserStory = new StructuredUserStory();
-		List<StructuredClass> classes = getClassesFromUserStory(taigaUserStory);
+		List<StructuredClass> classes = getClassesDataFromUserStory(taigaUserStory);
 		structuredUserStory.setClasses(classes);
-		
-//		for(StructuredClass structuredClass : classes) 
-//			System.out.println(structuredClass.getName() + structuredClass.getMethods().size());
+
+		// for(StructuredClass structuredClass : classes)
+		// System.out.println(structuredClass.getName() +
+		// structuredClass.getMethods().size());
 	}
 
-	private List<StructuredClass> getClassesFromUserStory(TaigaUserStory taigaUserStory) {
+	private List<StructuredClass> getClassesDataFromUserStory(TaigaUserStory taigaUserStory) {
 		List<StructuredClass> structuredClasses = new ArrayList<StructuredClass>();
 
 		List<String> statements = Arrays.asList(taigaUserStory.getDescription().split("\n"));
@@ -37,56 +40,94 @@ public class UserStoryParser {
 			statement = statement.trim();
 			matcher = classNamePattern.matcher(statement);
 			int i = 0;
+			boolean isASmpleStatement = true;
 			while (matcher.find()) {
+				isASmpleStatement = false;
 				StructuredClass parsedClass = new StructuredClass(matcher.group().trim());
 				if (!structuredClasses.contains(parsedClass)) {
 					structuredClasses.add(parsedClass);
-					StructuredConstrutor constructor = new StructuredConstrutor(parsedClass.getName(), null, null, null, new ArrayList<String>());
+					StructuredConstrutor constructor = new StructuredConstrutor(parsedClass.getName(), null, null, null,
+							new ArrayList<String>());
 					parsedClass.addConstructor(constructor);
 				}
 				int index = getIndexOfClass(structuredClasses, matcher.group().trim());
-				if(i++ == 0) //si c'est la première classe rencontrée dans le statement
-					if(statement.contains(Keywords.HAS.getName()))
+				if (i++ == 0) // si c'est la première classe rencontrée dans le
+								// statement
+					if (statement.contains(Keywords.HAS.getName()))
 						handleStatementAttribut(statement, structuredClasses.get(index));
 					else
 						handleStatementMethod(structuredClasses, statement, structuredClasses.get(index));
+			}
+			for (Keywords keyword : Keywords.values())
+				if (statement.equals(keyword.getName()))
+					isASmpleStatement = false;
+			if (isASmpleStatement) {
+				extractActionMethod(structuredClasses, statement);
 			}
 		}
 		return structuredClasses;
 	}
 
+	private void extractActionMethod(List<StructuredClass> structuredClasses, String statement) {
+		boolean methodAlreadyParsed = false;
+		String actionMethodName = StringUtils.stringAsMethodName(statement);
+		for(StructuredMethod method : userStoryClass.getMethods())
+			if(method.getName().equals(actionMethodName))
+				methodAlreadyParsed = true;
+		if(!methodAlreadyParsed) {
+			StructuredClass classArg = getClassNamed(structuredClasses, userStoryArgClassName);
+			StructuredMethod newMethod = null;
+			if(classArg == null)
+				newMethod = new StructuredMethod(actionMethodName, getMethodReturnType(statement), null);
+			else {
+				String[] args = new String[1];
+				args[0] = classArg.getName() + " " + StringUtils.uncapitalize(classArg.getName());
+				newMethod = new StructuredMethod(actionMethodName, getMethodReturnType(statement), args);
+			}
+			userStoryClass.addMethod(newMethod);
+		}
+	}
+
 	private int getIndexOfClass(List<StructuredClass> structuredClasses, String className) {
 		int index = 0;
-		for(int j = 0; j < structuredClasses.size(); j++)
-			if(structuredClasses.get(j).getName().equals(className))
+		for (int j = 0; j < structuredClasses.size(); j++)
+			if (structuredClasses.get(j).getName().equals(className))
 				index = j;
 		return index;
+	}
+	
+	private StructuredClass getClassNamed(List<StructuredClass> structuredClasses, String className) {
+		for(StructuredClass structuredClass : structuredClasses)
+			if(className.equals(structuredClass.getName()))
+					return structuredClass;
+		return null;
 	}
 
 	private void handleStatementAttribut(String statement, StructuredClass parsedClass) {
 		String single = Keywords.HAS.getName() + Keywords.ONE.getName();
-		String many = Keywords.HAS.getName() + Keywords.MANY.getName();;
+		String many = Keywords.HAS.getName() + Keywords.MANY.getName();
 		String none = Keywords.HAS.getName() + Keywords.NONE.getName();
-		String then = Keywords.THEN.getName(); 
+		String then = Keywords.THEN.getName();
 
 		String attributClassName = null;
 		StructuredAttribut structuredAttribut = null;
-		
+
 		if (statement.contains(single)) {
 			structuredAttribut = buildStructuredAttribut(statement, single, then, attributClassName);
-			//créer un statement dans les constructeurs de la classe qui initialise la valeur en appelant le constructeur vide
-			String attributInitializationstatement = "this." + structuredAttribut.getName() + " = new " + structuredAttribut.getType() + "();";
-			for(StructuredConstrutor structuredConstrutor : parsedClass.getConstructors())
+			// créer un statement dans les constructeurs de la classe qui
+			// initialise la valeur en appelant le constructeur vide
+			String attributInitializationstatement = "this." + structuredAttribut.getName() + " = new "
+					+ structuredAttribut.getType() + "();";
+			for (StructuredConstrutor structuredConstrutor : parsedClass.getConstructors())
 				structuredConstrutor.addStatement(attributInitializationstatement);
-		} else
-			if (statement.contains(many)) {
-				attributClassName = getAttributClassName(statement, many, then, attributClassName);
-				structuredAttribut = new StructuredAttribut("List<"+ attributClassName +">", StringUtils.uncapitalize(attributClassName + "s"));
-			} else
-				if(statement.contains(none))
-					structuredAttribut = buildStructuredAttribut(statement, none, then, attributClassName);
-					//on laisse l'attribut à null
-		
+		} else if (statement.contains(many)) {
+			attributClassName = getAttributClassName(statement, many, then, attributClassName);
+			structuredAttribut = new StructuredAttribut("List<" + attributClassName + ">",
+					StringUtils.uncapitalize(attributClassName + "s"));
+		} else if (statement.contains(none))
+			structuredAttribut = buildStructuredAttribut(statement, none, then, attributClassName);
+		// on laisse l'attribut à null
+
 		parsedClass.addAttribut(structuredAttribut);
 	}
 
@@ -101,19 +142,23 @@ public class UserStoryParser {
 	private String getAttributClassName(String statement, String many, String then, String attributClass) {
 		Pattern pattern = Pattern.compile(many + "(.*)" + then);
 		Matcher matcher = pattern.matcher(statement);
-		
+
 		if (matcher.find())
-			attributClass = statement.substring(statement.indexOf(many) + (many).length(), statement.length() - then.length()).trim();
+			attributClass = statement
+					.substring(statement.indexOf(many) + (many).length(), statement.length() - then.length()).trim();
 		return attributClass;
 	}
 
-	private void handleStatementMethod(List<StructuredClass> structuredClasses, String statement, StructuredClass parsedClass) {
+	private void handleStatementMethod(List<StructuredClass> structuredClasses, String statement,
+			StructuredClass parsedClass) {
 		StructuredMethod method = null;
-		if (statement.contains(Keywords.CAN.getName()))
+		if (statement.contains(Keywords.CAN.getName())) {
 			method = getUserStoryMethod(statement);
-		if(method != null && method.getName() != null && !method.getName().isEmpty())
+			userStoryClass = parsedClass;
+		}
+		if (method != null && method.getName() != null && !method.getName().isEmpty())
 			parsedClass.addMethod(method);
-		else 
+		else
 			getMethod(structuredClasses, statement);
 	}
 
@@ -124,55 +169,57 @@ public class UserStoryParser {
 		String[] args = new String[1];
 		String can = Keywords.CAN.getName();
 		String to = Keywords.TO.getName();
-		
+
 		if (statement.contains(to)) {
 			patternMethod = Pattern.compile(can + "(.*)" + to);
 			args[0] = statement.substring(statement.indexOf(to) + to.length()).trim();
-		}
-		else
+			userStoryArgClassName = args[0];
+		} else
 			patternMethod = Pattern.compile(can + "(.*)");
 		Matcher matcherMethod = patternMethod.matcher(statement);
 		while (matcherMethod.find())
-			methodName = StringUtils.stringAsMethodName(matcherMethod.group().substring(can.length()));
+			methodName = StringUtils.stringAsMethodName(matcherMethod.group().substring(can.length(), matcherMethod.group().length() - to.length())).trim();
 		StructuredMethod structuredMethod = new StructuredMethod(methodName, returnType, args);
-		
+
 		return structuredMethod;
 	}
-	
+
 	private void getMethod(List<StructuredClass> structuredClasses, String statement) {
 		String methodName = "";
 		String className = "";
 		Class<?> returnType = getMethodReturnType(statement);
 		List<String> args = new ArrayList<String>();
-	
-		System.out.println(statement + returnType);
-		//choper la classe dans laquelle on met la méthode
+
+		// choper la classe dans laquelle on met la méthode
 		Matcher matcher, matcherMethodName;
 		matcher = classNamePattern.matcher(statement);
-		if(matcher.find())
+		if (matcher.find())
 			className = matcher.group();
-		else 
+		else
 			return;
-		//choper ses arguments (les classes suivantes sur le statement)
-		while(matcher.find())
+		// choper ses arguments (les classes suivantes sur le statement)
+		while (matcher.find())
 			args.add(matcher.group().trim() + " " + StringUtils.uncapitalize(matcher.group().trim()));
 		String[] argsArray = new String[args.size()];
 		args.toArray(argsArray);
-		
-		//choper le nom de la méthode patternmethode (entre 2 classes ou entre classes et then si y'a rien entre la méthode et then)
-		String stmt = statement.substring(Keywords.IF.getName().length(), statement.length() - Keywords.THEN.getName().length());
-		
+
+		// choper le nom de la méthode patternmethode (entre 2 classes ou entre
+		// classes et then si y'a rien entre la méthode et then)
+		String stmt = statement.substring(Keywords.IF.getName().length(),
+				statement.length() - Keywords.THEN.getName().length());
+
 		Pattern patternMethodName = Pattern.compile(" ([a-z0-9])+ ");
 		matcherMethodName = patternMethodName.matcher(stmt);
-		
-		while(matcherMethodName.find()) {
+
+		while (matcherMethodName.find()) {
 			methodName = matcherMethodName.group().trim();
 		}
 		StructuredMethod structuredMethod = new StructuredMethod(methodName, returnType, argsArray);
 		StructuredClass newClass = new StructuredClass(className);
 		if (!structuredClasses.contains(newClass)) {
 			structuredClasses.add(newClass);
-			StructuredConstrutor constructor = new StructuredConstrutor(newClass.getName(), null, null, null, new ArrayList<String>());
+			StructuredConstrutor constructor = new StructuredConstrutor(newClass.getName(), null, null, null,
+					new ArrayList<String>());
 			newClass.addConstructor(constructor);
 			newClass.addMethod(structuredMethod);
 		} else
