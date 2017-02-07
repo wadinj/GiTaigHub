2ede1de2e37beea8a96274dc72be455fd82ee68e
Jo <Jo@10.19.1.159>
2017-02-07 21:19:18 +0100

    [Fix](Generator) remove compiling at runtime and use hard coded
    className

--- GiTaigHub/src/main/java/com/ww/core/Main.java
+++ GiTaigHub/src/main/java/com/ww/core/Main.java
@@ -7,16 +7,16 @@ import us_parser.UserStoryParser;
 public class Main {
 
 	public static void main(String[] args) {
-		String usernameTaiga = args[0];
-		String passwordTaiga = args[1];
-		String projectTaiga = args[2];
-		TaigaService service = new TaigaService(usernameTaiga, passwordTaiga, projectTaiga);
-		UserStoryParser userStoryParser = new UserStoryParser();
-		userStoryParser.parseUserStory(service.getUserStory("2"));
-		StructuredUserStory structuredUs = userStoryParser.getStructuredUserStory();
-		CodeGeneratorService generator = new CodeGeneratorService();
-		generator.generateCodeFromStructuredUserStory(structuredUs);
-		System.out.println(new ThesaurusService().getSynonymousOfWord("can"));
+//		String usernameTaiga = args[0];
+//		String passwordTaiga = args[1];
+//		String projectTaiga = args[2];
+//		TaigaService service = new TaigaService(usernameTaiga, passwordTaiga, projectTaiga);
+//		UserStoryParser userStoryParser = new UserStoryParser();
+//		userStoryParser.parseUserStory(service.getUserStory("1"));
+//		StructuredUserStory structuredUs = userStoryParser.getStructuredUserStory();
+//		CodeGeneratorService generator = new CodeGeneratorService();
+//		generator.generateCodeFromStructuredUserStory(structuredUs);
+		System.out.println(new ThesaurusService().getSynonymousOfWord("have").getVerb().getSyn().toString());
 	}
 
 }
